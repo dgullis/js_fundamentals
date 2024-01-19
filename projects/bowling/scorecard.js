5a9a64219e04340a850825463789e24719fb6d16
dan gullis <daniel.gullis1@gmail.com>
2024-01-19 15:36:38 +0000

    altered code structure, started to implement endOfGame function

--- projects/bowling/scorecard.js
+++ projects/bowling/scorecard.js
@@ -1,14 +1,167 @@
 class Scorecard {
     constructor() {
         this.score = 0;
-        this.frames = [];
-        this.previous_frame_score = [];
+        this.frames = 0;
+        this.memory = [];
     };
 
     calculateScore() {
         return this.score;
     };
 
+    returnNumberFrames() {
+        return this.frames;
+    }
+
+    addFrame() {
+        let roll1 = arguments[0];
+        let roll2 = arguments[1];
+        
+        this.handleEndOfGame(roll1, roll2)
+        // if strike
+        if (roll1 === 10){
+            this.handleStrike();
+            this.frames += 1;
+        // if not a strike check if spare        
+        } else if ((roll1 + roll2) === 10) {
+            this.handleSpare(roll1, roll2);
+            this.frames += 1;
+
+        // if not strike or spare   
+        } else {
+            this.handleNormalRoll(roll1, roll2)
+            this.frames += 1;
+
+        };
+    };
+
+    
+    handleStrike() {
+        // check to see uncalculated scores i.e. spares/strike waiting to be settled
+        if(this.memory.length !== 0){
+            // if one score waiting to be settled
+            if (this.memory.length === 1){
+                // if this is a strike, add second strike
+                if(this.memory[0] === (10)){
+                    this.memory.push(10);
+                // if not strike i.e. spare, settle this score, delete spare, add strike 
+                } else {
+                    this.score += 20;
+                    this.memory.shift();
+                    this.memory.push(10)
+                }
+            // if two scores waiting to be settled (must be strikes)    
+            } else if (this.memory.length === 2) {
+               // settles first score (30), delete first score
+                    this.score += 30;
+                    this.memory.push(10)
+                    this.memory.shift();  
+            }
+        // if no scores waiting to be settled, add strike to memory
+        } else {
+            this.memory.push(10);
+        }
+    };
+
+    handleSpare(roll1, roll2) {
+        // check if scores waiting to be settled
+        if (this.memory.length !== 0) {
+            // if one score waiting to be settled
+            if (this.memory.length === 1) {
+                // if strike waiting to be settled, settle score, delete strike, add spare
+                if (this.memory[0] == 10) {
+                    this.score += 20;
+                    this.memory.shift();
+                    this.memory.push([roll1,roll2])
+                // if not a strike, must be a spare waiting to be settled, settle score, delete spare, add current spare
+                } else {
+                    this.score += (10 + roll1);
+                    this.memory.shift();
+                    this.memory.push([roll1,roll2])
+                }
+            // if length is 2 must be two strike?, settle both strike, add spare    
+            } else if ( this.memory.length === 2 ) {
+                this.score += (40 + roll1);
+                this.memory.shift();
+                this.memory.shift();
+                this.memory.push([roll1, roll2]);
+            }
+        // if no scores waiting to be settled
+        } else {
+            this.memory.push([roll1,roll2]);
+        }
+    };
+
+    handleNormalRoll(roll1, roll2) {
+        if (this.memory.length !== 0) {
+            // if one waiting
+            if (this.memory.length === 1) {
+                // if strike, settle strike, add strike w/ bonus to score, delete strike, add current rolls to score
+                if (this.memory[0] === 10) {
+                    this.score += (10 + (roll1+roll2) + (roll1+roll2));
+                    this.memory.shift();
+                // if not strike must be spare
+                } else {
+                    this.score += (10 + roll1 + (roll1 + roll2));
+                    this.memory.shift();
+                }
+            // if two scores waiting to be settled (must be 2 x strikes?), settle scores, delete from memory, add current score    
+            } else if ( this.memory.length === 2 ) {
+                this.score += 20 + (roll1) + 10 + (roll1 + roll2) + (roll1 + roll2);
+                this.memory.shift();
+                this.memory.shift();
+            }
+        // if no scores waiting to be settled just add current rolls to score    
+        } else {
+            this.score += (roll1 + roll2);
+        };
+    };
+
+    handleEndOfGame(roll1, roll2) {
+        if (this.frames === 10) {
+            if (roll1+roll2 === 10) {
+                this.handleSpare(roll1, roll2)
+                console.log("end of game")
+            } else if (roll1===10) {
+                this.handleStrike(roll1);
+                this.frames += 1
+
+            } else {
+                // if one waiting
+                if (this.memory.length === 1) {
+                    // if strike, settle strike, add strike w/ bonus to score, delete strike, add current rolls to score
+                    if (this.memory[0] === 10) {
+                        this.score += (10 + (roll1+roll2) + (roll1+roll2));
+                        this.memory.shift();
+                    // if not strike must be spare
+                    } else {
+                        this.score += (10 + roll1 + (roll1 + roll2));
+                        this.memory.shift();
+                    }
+                // if two scores waiting to be settled (must be 2 x strikes?), settle scores, delete from memory, add current score    
+                } else if ( this.memory.length === 2 ) {
+                    this.score += 20 + (roll1) + 10 + (roll1 + roll2)
+                    this.memory.shift();
+                    this.memory.shift();
+                }
+            console.log('end of game')
+            }
+        } else if (this.frames === 11){
+            if (this.memory.slice(-1)[0] != 10) {
+                this.score += 10 + roll1
+                console.log('end of game')
+            } else {
+                this.score += 20 + roll1
+                console.log("end of game")
+            }
+        } else if (this.frames === 12){
+            if (roll1 === 10) {
+                this.score += (this.memory[0] + this.memory[1] + 10)
+            } else {
+                this.score += (this.memory[0] + this.memory[1] + roll1)
+            }
+        }
+    }
 };
 
 module.exports = Scorecard;
--- projects/bowling/scorecard.test.js
+++ projects/bowling/scorecard.test.js
@@ -5,4 +5,93 @@ describe('unit tests for scorecard', () => {
         const scorecard = new Scorecard();
         expect(scorecard.calculateScore()).toEqual(0);
     });
-})
+    test('(5,2) equal 7', () => {
+        const scorecard = new Scorecard();
+        scorecard.addFrame(5,2);
+        expect(scorecard.calculateScore()).toEqual(7);
+    });
+    test('stike - (5,2) equal 24', () => {
+        const scorecard = new Scorecard();
+        scorecard.addFrame(10);
+        scorecard.addFrame(5,2);
+        expect(scorecard.calculateScore()).toEqual(24);
+    });
+    test('strike, spare equal 20', () => {
+        const scorecard = new Scorecard();
+        scorecard.addFrame(10);
+        scorecard.addFrame(1,9)
+        expect(scorecard.calculateScore()).toEqual(20);
+    });
+    test('strike, strike, spare then 8 equal 26', () => {
+        const scorecard = new Scorecard();
+        scorecard.addFrame(10);
+        scorecard.addFrame(7,1);
+        expect(scorecard.calculateScore()).toEqual(26);
+    });
+    test('strike, strike, spare, spare equal 68', () => {
+        const scorecard = new Scorecard();
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        scorecard.addFrame(9,1);
+        scorecard.addFrame(9,1);
+        expect(scorecard.calculateScore()).toEqual(68);
+    });
+    test('spare, spare, strike, (5,2) equal 63', () => {
+        const scorecard = new Scorecard();
+        scorecard.addFrame(9,1);
+        scorecard.addFrame(9,1);
+        scorecard.addFrame(10);
+        scorecard.addFrame(5,2);
+        expect(scorecard.calculateScore()).toEqual(63);
+    });
+    test('13 strikes equal 300', () => {
+        const scorecard = new Scorecard();
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        expect(scorecard.calculateScore()).toEqual(300);
+    });
+    test('random score equal', () => {
+        const scorecard = new Scorecard();
+        scorecard.addFrame(2,5);
+        scorecard.addFrame(8,1);
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        scorecard.addFrame(10);
+        scorecard.addFrame(5,1);
+        scorecard.addFrame(3,7);
+        scorecard.addFrame(9,0);
+        scorecard.addFrame(2,8);
+        scorecard.addFrame(7,3);
+        scorecard.addFrame(1,9);
+
+        expect(scorecard.calculateScore()).toEqual(149);
+    });
+//     test('strike, strike, 5,1 equals 47', () => {
+//         const scorecard = new Scorecard();
+//         scorecard.addFrame(10);
+//         scorecard.addFrame(10);
+//         scorecard.addFrame(5,1);
+//         expect(scorecard.calculateScore()).toEqual(47);
+//     });
+// //     test('strike, strike, strike, (6, 2) equals 82', () => {
+// //         const scorecard = new Scorecard();
+// //         scorecard.addFrame(10);
+// //         scorecard.addFrame(10);
+// //         scorecard.addFrame(10);
+// //         scorecard.addFrame(6,2)
+// //         expect(scorecard.calculateScore()).toEqual(82);
+
+
+    });
+
+

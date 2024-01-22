30a36a2b1f21b0b4a08aeb9635c4d0d7776268d9
dan gullis <daniel.gullis1@gmail.com>
2024-01-22 15:40:37 +0000

    react test-project component (Profile) test

---
+++ test-project/src/Profile.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Profile = () => {
+
+    return (
+        <div id="profile">
+            <h1 id="name">Quackie Makers</h1>
+
+            <p id="job">Makers' favourite rubber duck</p>
+            <p id="birthdate">2013</p>
+            <p id="sound">quack quack</p>
+        </div>
+    )
+}
+
+export default Profile;
--- test-project/src/assets/Profile.jsx
+++
@@ -1,6 +0,0 @@
-<div id="profile">
-    <h1 id="name">Quackie Makers</h1>
-
-    <p id="job">Makers' favourite rubber duck</p>
-    <p id="birthdate">2013</p>
-</div>
